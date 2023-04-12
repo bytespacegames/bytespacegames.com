babe14b4fec5f9b51e2bd15cde8b153e8a0b0aa2
space <55336451+bytespacegames@users.noreply.github.com>
2023-04-11 20:35:28 -0400

    cookies still dont work

--- switches/switches.js
+++ switches/switches.js
@@ -75,9 +75,14 @@ function draw() {
   if (hovering(5,height - 15 - 5,50,15)) {
     fill(245)    
     if (mouseIsPressed && mouseButton === LEFT) {
-      document.cookie.split(";").forEach(function(c) {
-		document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
-	  });
+      var cookies = document.cookie.split(";");
+
+	  for (var i = 0; i < cookies.length; i++) {
+		var cookie = cookies[i];
+		var eqPos = cookie.indexOf("=");
+		var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+		document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+	  }
       window.location.reload();
     }
   }
