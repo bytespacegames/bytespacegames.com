aa0efa9f9cdba994f2ea3ca4519eaa7149d52ad7
space <55336451+bytespacegames@users.noreply.github.com>
2023-04-11 20:40:00 -0400

    hthrth

--- switches/switches.js
+++ switches/switches.js
@@ -81,7 +81,7 @@ function draw() {
 		var cookie = cookies[i];
 		var eqPos = cookie.indexOf("=");
 		var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-		document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+		document.cookie = name + "=0;expires=Thu, 01 Jan 1970 00:00:00 GMT";
 	  }
       window.location.reload();
     }
